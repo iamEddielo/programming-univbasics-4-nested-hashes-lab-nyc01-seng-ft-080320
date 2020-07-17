578764cff061f086b8d031c809b01d540403430d
Eddie Lopez <iameddielopez@gmail.com>
2020-07-17 23:10:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:languages] = "Assembly"\
+  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  
   programmer_hash
 
 end
